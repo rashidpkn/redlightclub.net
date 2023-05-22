c2cc5355bbb96c582290040b530d7b20bf2e1be5
Muhammed Rashid N <muhammed916rashid@gmail.com>
2023-05-22 17:49:19 +0530

    Somthing is added

--- frontend/src/pages/home/components/Screen1.jsx
+++ frontend/src/pages/home/components/Screen1.jsx
@@ -10,8 +10,10 @@ function Screen1() {
     const [country, setCountry] = useState('')
     const dispatch = useDispatch()
     const {region} = useSelector(state=>state.util)
+    const [countryCode, setCountryCode] = useState('')
     useEffect(() => {
             axios.get('https://api.country.is').then(res => {
+                setCountryCode(res.data.country)
                     setCountry(states.find(e=>e.iso2 === res.data.country).name)
                     // setState(state.filter(e=>e.iso== res.data.country && e.state.name))
                     setState(states.find(e=>e.iso2=== res.data.country).states);
@@ -23,7 +25,10 @@ function Screen1() {
     return (
         <div className='h-screen bg-cover bg-center bg-no-repeat relative' style={{ backgroundImage: "url('/images/home/Home banner-2.png')" }}>
             <div className="absolute top-56 right-0  w-96 bg-black/20 p-4 text-white space-y-2 rounded-lg backdrop-blur-md">
-                <h2 className='text-lg font-medium'>Hey there, Are you from {country}?</h2>
+                <div className="flex items-center gap-3"> 
+                <img src={`https://flagsapi.com/${countryCode}/flat/64.png`} width={40} alt="" />
+                <h2 className='text-lg font-medium'>Hey there, Are you from {country}? </h2> 
+                </div>
                 <p className='text-[13px]'>Choose from these recommended locations</p>
                 <div className="flex gap-2 overflow-y-auto">
                     {state.map(e=><button className={` px-2 h-12 min-w-fit border border-white rounded-sm ${region === e.name && 'border-yellow-300'}`} onClick={()=>{dispatch(setRegion(e.name))}}>{e.name}</button>)}
